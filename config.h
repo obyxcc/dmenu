4742ed27805be2437f52612e2b9a82719d665e56
cole@obyx.cc <cole@obyx.cc>
2023-10-20 16:50:44 -0400

    font changes

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static int min_width = 800;                /* minimum width when centered */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMono Nerd Font:style=Bold:size=12"
+	"JetBrainsMono Nerd Font:style=Bold:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -25,7 +25,7 @@ static const char *colors[SchemeLast][2] = {
 static unsigned int lines      = 18;
 
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 38;
+static unsigned int lineheight = 34;
 static unsigned int min_lineheight = 8;
 
 /*
